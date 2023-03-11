02265ba6313fbcc3e6cd257bcf587e92a126c98e
JUAN SEBASTIAN SANCHEZ L <js.sanchezl12@uniandes.edu.co>
2023-03-10 19:22:26 -0500

    Cambios Create Alarma Clasica

--- Maquetacion_WEB/TTMProject/src/app/create/create.component.ts
+++ Maquetacion_WEB/TTMProject/src/app/create/create.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
 
+  alarmType: string = 'Clásica';
+  sonido: string = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeAlarmType(alarmType_: string) {
+    console.log(alarmType_);
+    this.alarmType = alarmType_;
+  }
+
+  changeSonido(sonido_: string) {
+    console.log(sonido_);
+    this.sonido = "Sonido " + sonido_;
   }
 
 }
